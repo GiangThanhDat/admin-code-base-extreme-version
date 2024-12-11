4efb68334419aa263c9a46cddc065428031814ad
Giang Thanh Dat <gthanhdatpro@gmail.com>
2024-12-11 17:25:37 +0700

    fix: filter not in

--- app/demo-grid.tsx
+++ app/demo-grid.tsx
@@ -11,7 +11,6 @@ import {
   SearchPanel,
   Editing,
   Summary,
-  GroupItem,
   Toolbar,
   Item,
   Grouping,
@@ -22,10 +21,17 @@ import {
   HeaderFilter,
   Search,
   FilterPanel,
+  GroupPanel,
+  Button as GridButton,
+  ColumnChooserSearch,
+  ColumnChooserSelection,
+  StateStoring,
+  DataGridRef,
+  TotalItem,
 } from "devextreme-react/data-grid";
 import { FilterDescriptor } from "devextreme/data";
 import CustomStore from "devextreme/data/custom_store";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 type Unit = {
   id: number;
@@ -61,37 +67,44 @@ export const capitalizeFirstLetter = (string: string) => {
   return (string && string[0].toUpperCase() + string.slice(1)) || "";
 };
 
+const processNegations = (negationGroup: [string, string, unknown][]) => {
+  for (const subFilter of negationGroup)
+    if (Array.isArray(subFilter)) {
+      subFilter[1] = "!=";
+    }
+};
+
 const createFilterColumn = (filter: FilterDescriptor | FilterDescriptor[]) => {
   const map: Record<
     string,
     { column: string; expression: string; keySearch: string[] }
   > = {};
   if (!filter) return [];
-
-  if (!Array.isArray(filter[0])) {
-    const [column, expression, keySearch] = filter;
-    return [
-      {
-        column: capitalizeFirstLetter(column),
-        expression,
-        keySearch: keySearch.toString(),
-      },
-    ];
-  }
-
+  // try {
   const queue = [...filter];
 
   for (let i = 0; i < queue.length; i++) {
     const item = queue[i];
-    if (typeof item[0] === "string" && !["a", "o"].includes(item[0])) {
+
+    if (item === "!" && queue[i + 1]) {
+      processNegations(queue[i + 1]);
+    }
+
+    if (item[0] === "!" && item[1]) {
+      processNegations(item[1]);
+    }
+
+    if (typeof item[0] === "string" && !["a", "o", "!"].includes(item[0])) {
       const [column, expression, keySearch] = item;
 
       if (!keySearch) continue;
 
+      const operation = expression === "=" ? "IN" : "NOT IN";
+
       if (!map[column]) {
         map[column] = {
           column: capitalizeFirstLetter(column),
-          expression: expression === "=" ? "IN" : expression,
+          expression: operation,
           keySearch: [keySearch],
         };
       } else {
@@ -106,19 +119,19 @@ const createFilterColumn = (filter: FilterDescriptor | FilterDescriptor[]) => {
     ...item,
     keySearch: `(${item.keySearch.toString()})`,
   }));
+  // } catch (error) {
+  //   console.log("error:", error);
+  // }
 };
 
 const token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcmltYXJ5c2lkIjoiNTkiLCJ1bmlxdWVfbmFtZSI6ImFkbWluIiwiZ3JvdXBzaWQiOiIxMiIsIkludm9pY2VDb25maWdJZCI6IjAiLCJCcmFuY2hJZHMiOiIiLCJTdGFmZklkIjoiMzMiLCJXYXJlaG91c2VJZHMiOiIiLCJVc2VyQ2FzaGllcklkcyI6IjIwMCw1OSIsIlBheW1lbnRUeXBlSWQiOiIyIiwibmJmIjoxNzMzNzM3MzUxLCJleHAiOjE3MzYzMjkzNTEsImlhdCI6MTczMzczNzM1MX0.Zo-Hu5CRQjEMzb-5S-0R7LPgzjZFFlzeCHl3DmqsBLI";
 
 const customDataSource = new CustomStore({
-  // key: "id",
+  key: "id",
   load: async (loadOptions) => {
-    // console.log("loadOptions:", loadOptions);
+    console.log("are you fine?");
     const { skip = 0, take = 1, filter } = loadOptions;
-
-    // Con vu ['!', [array]] chua xu ly duoc
-    console.log("filter:", filter);
     const filterColumn = createFilterColumn(filter) || [
       { column: "string", keySearch: "string", expression: "string" },
     ];
@@ -168,7 +181,7 @@ const unitStore = new CustomStore({
       keySearch: searchValue || "",
       pageIndex: `${skip / take + 1}`,
       pageSiez: `${take}`,
-      isGetAll: "false",
+      isGetAll: "true",
     }).toString();
 
     return fetch(
@@ -206,7 +219,7 @@ const warehouseStore = new CustomStore({
       id: "0",
       keySearch: searchValue || "",
       pageIndex: `${skip / take + 1}`,
-      pageSiez: `${take}`,
+      pageSize: `${take}`,
       isGetAll: "false",
     }).toString();
 
@@ -255,103 +268,192 @@ const warehouseHeaderFilterSource = {
 
 export const DemoGrid = () => {
   const [expanded, setExpanded] = useState(true);
+  const dataGridRef = useRef<DataGridRef>(null);
+
   return (
-    <DataGrid
-      id="dataGrid"
-      dataSource={customDataSource}
-      columnAutoWidth
-      allowColumnResizing
-      allowColumnReordering
-      showBorders
-      showRowLines
-    >
-      <RemoteOperations
-        filtering
-        // paging
-        // sorting
-        // summary
-        // grouping
-        groupPaging
-      />
-      <ColumnChooser enabled={true} mode="select" />
-      <Selection
-        mode="multiple"
-        selectAllMode="allPages"
-        showCheckBoxesMode="always"
-      />
-      <FilterRow visible={true} showOperationChooser />
-      <Scrolling mode="standard" rowRenderingMode="standard" />
-      <Paging enabled={true} defaultPageSize={10} />
-      <Pager
-        visible={true}
-        showInfo
-        showNavigationButtons
-        showPageSizeSelector
-        displayMode="adaptive"
-        allowedPageSizes={"auto"}
-      />
-      <HeaderFilter visible={true}>
-        <Search enabled={true} mode="contains" />
-      </HeaderFilter>
-      <SearchPanel visible={true} />
-      <FilterPanel visible={true} />
-      {/* <Column dataField="branchName" /> */}
-      <Column dataField="code" caption="Ma hang hoa" />
-      <Column dataField="note" caption="Ghi chu" />
-      <Column dataField="name" caption="Ten hang hoa" />
-      <Column dataField="priceBuy" dataType="number" caption="Gia mua" />
-      <Column dataField="priceBuy1" dataType="number" caption="Gia mua 1" />
-      <Column dataField="priceBuy2" dataType="number" caption="Gia mua 2" />
-      <Column dataField="priceSale" dataType="number" caption="Gia ban" />
-      <Column dataField="priceSale1" dataType="number" caption="Gia ban 1" />
-      <Column dataField="priceSale2" dataType="number" caption="Gia ban 2" />
-      <Column
-        dataField="unitId"
-        dataType="string"
-        cellRender={(props) => <>{props.data.unitName}</>}
-        caption="Don vi tinh"
+    <>
+      <DataGrid
+        keyExpr={"id"}
+        ref={dataGridRef}
+        id="dataGrid"
+        dataSource={customDataSource}
+        columnAutoWidth
+        allowColumnResizing
+        allowColumnReordering
+        showBorders
+        showRowLines
+        rowAlternationEnabled
+        // columnHidingEnabled={true}
       >
-        <HeaderFilter dataSource={unitHeaderFilterSource} />
-      </Column>
-      <Column dataField="unit1Name" caption="Don vi tinh 1">
-        <HeaderFilter dataSource={unitHeaderFilterSource} />
-      </Column>
-      <Column dataField="unit2Name" caption="Don vi tinh 2">
-        <HeaderFilter dataSource={unitHeaderFilterSource} />
-      </Column>
-      <Column dataField="warehouseName" caption="Kho hang">
-        <HeaderFilter dataSource={warehouseHeaderFilterSource} />
-      </Column>
-      <Column dataField="weight" dataType="number" caption="Khoi luong" />
-      <Column dataField="weight1" dataType="number" caption="Khoi luong 1" />
-      <Column dataField="weight2" dataType="number" caption="Khoi luong 2" />
-      <Column dataField="img" caption="Thumb" />
-      <ColumnFixing enabled={true} />
-      <Editing
-        mode="popup"
-        allowUpdating={true}
-        allowDeleting={true}
-        allowAdding={true}
-      />
-      {/* <Editing mode="form" allowUpdating allowAdding /> */}
-      {/* <Editing mode="row" allowUpdating /> */}
-      <Summary>
-        <GroupItem summaryType="count" />
-      </Summary>
-      <Toolbar>
-        <Grouping autoExpandAll={expanded} />
-        <Item name="groupPanel" />
-        <Item location="after">
-          <Button
-            text={expanded ? "Collapse All" : "Expand All"}
-            width={136}
-            onClick={() => setExpanded((prevExpanded) => !prevExpanded)}
+        <RemoteOperations
+          filtering
+          // paging
+          // sorting
+          // summary
+          groupPaging
+          // grouping
+        />
+        <ColumnChooser enabled={true} mode="select">
+          <ColumnChooserSearch
+            enabled
+            editorOptions={{ placeholder: "Tim ten cot" }}
           />
-        </Item>
-        <Item name="addRowButton" />
-        <Item name="columnChooserButton" />
-        <Item name="searchPanel" />
-      </Toolbar>
-    </DataGrid>
+          <ColumnChooserSelection allowSelectAll selectByClick recursive />
+        </ColumnChooser>
+        <Selection mode="multiple" deferred />
+        <FilterRow visible={true} showOperationChooser />
+        <GroupPanel visible={true} />
+        <Scrolling mode="standard" rowRenderingMode="standard" />
+        <Paging enabled={true} defaultPageSize={10} />
+        <Pager
+          visible={true}
+          showInfo
+          showNavigationButtons
+          showPageSizeSelector
+          displayMode="adaptive"
+          allowedPageSizes={"auto"}
+        />
+        <StateStoring
+          enabled
+          type="localStorage"
+          storageKey="APPLICATION_STORAGE"
+        />
+        <HeaderFilter visible={true}>
+          <Search enabled={true} mode="contains" />
+        </HeaderFilter>
+        <SearchPanel visible={true} />
+        <FilterPanel visible={true} />
+        <Editing
+          mode="popup"
+          allowUpdating
+          allowDeleting
+          allowAdding
+          useIcons
+        />
+        <Column type="buttons" width={"auto"}>
+          <GridButton name="edit" />
+          <GridButton name="delete" />
+          <GridButton hint="clone" icon="copy" />
+        </Column>
+        {/* <Editing mode="form" allowUpdating allowAdding /> */}
+        {/* <Editing mode="row" allowUpdating /> */}
+        <Summary>
+          {/* <GroupItem summaryType="count" /> */}
+          <TotalItem
+            column="priceBuy"
+            summaryType="sum"
+            valueFormat="currency"
+          />
+          <TotalItem
+            column="priceBuy1"
+            summaryType="sum"
+            valueFormat={"currency"}
+          />
+          <TotalItem
+            column="priceBuy2"
+            summaryType="sum"
+            valueFormat={"currency"}
+          />
+        </Summary>
+        <Toolbar>
+          <Grouping autoExpandAll={expanded} />
+          <Item name="groupPanel" />
+          <Item location="after">
+            <Button
+              text={expanded ? "Collapse All" : "Expand All"}
+              width={136}
+              onClick={() => setExpanded((prevExpanded) => !prevExpanded)}
+            />
+          </Item>
+          <Item>
+            <Button
+              type="normal"
+              text="Reset"
+              onClick={() => dataGridRef?.current?.instance().state(null)}
+            />
+          </Item>
+          <Item name="addRowButton" />
+          <Item name="columnChooserButton" />
+          <Item name="searchPanel" />
+        </Toolbar>
+
+        {/* <Column dataField="branchName" /> */}
+        <Column dataField="code" caption="Mã hàng hóa" />
+        <Column dataField="note" caption="Ghi chú" />
+        <Column dataField="name" caption="Tên hàng hóa" />
+        <Column
+          dataField="priceBuy"
+          dataType="number"
+          alignment="right"
+          format=",##0.##"
+          caption="Giá mua"
+        />
+        <Column
+          dataField="priceBuy1"
+          dataType="number"
+          alignment="right"
+          format=",##0.##"
+          caption="Giá mua 1"
+        />
+        <Column
+          dataField="priceBuy2"
+          dataType="number"
+          alignment="right"
+          format=",##0.##"
+          caption="Giá mua 2"
+        />
+        <Column
+          dataField="priceSale"
+          dataType="number"
+          alignment="right"
+          format=",##0.##"
+          caption="Giá bán"
+        />
+        <Column
+          dataField="priceSale1"
+          dataType="number"
+          alignment="right"
+          format=",##0.##"
+          caption="Giá bán 1"
+        />
+        <Column
+          dataField="priceSale2"
+          dataType="number"
+          alignment="right"
+          format=",##0.##"
+          caption="Giá bán 2"
+        />
+        <Column
+          dataField="unitId"
+          dataType="string"
+          cellRender={(props) => <>{props.data.unitName}</>}
+          caption="Don vi "
+        >
+          <HeaderFilter dataSource={unitHeaderFilterSource} />
+        </Column>
+        <Column
+          dataField="unit1Id"
+          caption="Đơn vị tính 1"
+          cellRender={(props) => <>{props.data.unit1Name}</>}
+        >
+          <HeaderFilter dataSource={unitHeaderFilterSource} />
+        </Column>
+        <Column
+          dataField="unit2Id"
+          cellRender={(props) => <>{props.data.unit2Name}</>}
+          caption="Đơn vị tính 2"
+        >
+          <HeaderFilter dataSource={unitHeaderFilterSource} />
+        </Column>
+        <Column dataField="warehouseId" caption="Kho hàng">
+          <HeaderFilter dataSource={warehouseHeaderFilterSource} />
+        </Column>
+        <Column dataField="weight" dataType="number" caption="Khối lượng" />
+        <Column dataField="weight1" dataType="number" caption="Khối lượng 1" />
+        <Column dataField="weight2" dataType="number" caption="Khối lượng 2" />
+        <Column dataField="img" caption="Ảnh minh họa" />
+        <ColumnFixing enabled={true} />
+      </DataGrid>
+    </>
   );
 };
